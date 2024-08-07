dcd2facd9d323879673e181856cbdf308b13a634
Bayu Sutra <202111224bayus@gmail.com>
2024-08-07 11:35:56 +0700

    update-dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
                     <a class="nav-link" href="/history">Riwayat</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/contact">Kontak</a>
+                    <a class="nav-link" href="/contact">Layanan</a>
                 </li>
             </ul>
         </div>
